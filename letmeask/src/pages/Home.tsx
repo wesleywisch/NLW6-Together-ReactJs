2dc7f7c4b8c5318599a6872b450fa5f61f04a0de
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-21 22:45:16 -0400

    Utilizando a contextApi, recuperando os dados de autenticação e criando o hook de autenticação

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -7,11 +7,17 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home(){
     const history = useHistory();
+    const { user, signInWithGoogle } = useAuth()
 
-    function navigateToNewRoom(){
+    async function handleCreateRoom(){
+        if(!user){
+            await signInWithGoogle();
+        }
+
         history.push('/rooms/new')
     }
 
@@ -29,7 +35,7 @@ export function Home(){
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
 
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
