7a85ea44d9616a4cfbae904c0aff8b4215852486
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-22 21:01:24 -0400

    Criado o fluxo de criação de salas e a entrada das salas

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoo";
+import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
 
 
 function App() {
-  
+
 
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
---
+++ letmeask/src/pages/Room.tsx
@@ -0,0 +1,8 @@
+
+
+
+export function Room(){
+    return(
+        <h1>Sala</h1>
+    )
+}
