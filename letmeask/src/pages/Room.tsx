67873200aec98015c5051deaf33be07c284d9b00
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-23 20:40:58 -0400

    Estrutura do html e css das perguntas estão prontas

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -5,6 +5,7 @@ import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question/Question';
 
 import { useAuth } from '../hooks/useAuth';
 
@@ -22,7 +23,7 @@ type FirebaseQuestions = Record<string, {
     isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
     id: string;
     author: {
         name: string;
@@ -41,7 +42,7 @@ export function Room(){
     const { user } = useAuth()
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTitle] = useState('');
 
     const roomId = params.id
@@ -129,7 +130,17 @@ export function Room(){
                     </div>
                 </form>
 
-                {JSON.stringify(questions)}
+                <div className="question-list">
+                {questions.map(question =>{
+                    return(
+                        <Question
+                        key={question.id} 
+                        content={question.content}
+                        author={question.author}
+                        />
+                    )
+                })}
+                </div>
             </main>
         </div>
     )
